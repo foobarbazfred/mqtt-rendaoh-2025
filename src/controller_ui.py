8d3de8fa8202095e1e24c8a3d46dcc4ce3d041b4
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-08-16 21:10:43 +0900

    Create controller_ui.py

---
+++ src/controller_ui.py
@@ -0,0 +1,39 @@
+#
+# ui class for controller (for M5STACK S3ATOM)
+# v0.01 2025/8/16  1st
+#
+
+#
+# copy from following repository
+# https://github.com/russhughes/st7789py_mpy/tree/master
+import tft_config
+import st7789py as st7789
+import vga1_8x8
+
+
+LCD_WIDTH_SIZE = 128
+LCD_HEIGHT_SIZE = 128
+
+class ControllerUI:
+
+    def __init__(self):
+        self.lcd = tft_config.config(tft_config.WIDE)
+        self.lcd_clear()
+        self.UI_TEXT1_POS = (0, 10)
+        self.UI_TEXT2_POS = (0, 20)
+        self.UI_TEXT3_POS = (0, 30)
+
+    def lcd_clear(self, color = st7789.BLACK):
+        self.lcd.fill(color)
+
+    def lcd_draw_text(self, x, y, text, fg=st7789.WHITE, bg=st7789.BLACK):
+        font_height = 8
+        self.lcd.fill_rect(x, y, LCD_WIDTH_SIZE, font_height, bg)
+        self.lcd.text(vga1_8x8, text, x, y, fg, bg)
+
+    def lcd_draw_line(self, x0, y0, x1, y1, color=st7789.WHITE):
+        self.lcd.line(x0, y0, x1, y1, color)
+
+    def lcd_fill_rect(self, x, y, w, h, color=st7789.BLACK):
+        self.lcd.fill_rect(x, y, w, h, color)
+
